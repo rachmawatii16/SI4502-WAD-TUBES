56c6c71f18e461847a14ed8a1d59a61e112737ef
raihanaryoo <raihanaryo92@gmail.com>
2023-12-31 22:39:28 +0700

    komit8

--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -1,28 +1,47 @@
 @extends('layoutAdmin')
 @section('title', 'Admin Orders')
 @section('content')
-    <h2>All Orders</h2>
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
-    @foreach($orders as $order)
-        <div>
-            <form method="POST" action="{{ route('admin.order.update', ['id' => $order->id]) }}">
-                @csrf
-                @method('PUT')
-                <div class="form-group">
-                    <label for="status">Update Status:</label>
-                    <select id="status" name="status" class="form-control" required>
-                        <option value="In process" {{ $order->status == 'In process' ? 'selected' : '' }}>In process</option>
-                        <option value="On the way" {{ $order->status == 'On the way' ? 'selected' : '' }}>On the way</option>
-                        <option value="Food Arrived" {{ $order->status == 'Food Arrived' ? 'selected' : '' }}>Food Arrived</option>
-                    </select>
+    <div class="container mt-4">
+        <h2 class="mb-4 text-center">All Orders</h2>
+
+        @if(session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        <div class="list-group">
+            @forelse($orders as $order)
+                <div class="list-group-item">
+                    <h5 class="mb-3">Order ID: {{ $order->id }}</h5>
+                    <p><strong>User:</strong> {{ $order->user->name ?? 'User not found' }}</p>
+                    <p><strong>Food Name:</strong> {{ $order->food_name }}</p>
+                    <p><strong>Current Status:</strong> {{ $order->status }}</p>
+                    <a href="{{ route('admin.orders.edit', $order->id) }}" class="btn btn-info mb-2">Edit Order</a>
+                    <form method="POST" action="{{ route('admin.order.update', ['id' => $order->id]) }}" class="d-inline">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="status-{{ $order->id }}">Update Status:</label>
+                            <select id="status-{{ $order->id }}" name="status" class="form-control">
+                                <option value="In process" {{ $order->status == 'In process' ? 'selected' : '' }}>In process</option>
+                                <option value="On the way" {{ $order->status == 'On the way' ? 'selected' : '' }}>On the way</option>
+                                <option value="Food Arrived" {{ $order->status == 'Food Arrived' ? 'selected' : '' }}>Food Arrived</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Update Status</button>
+                    </form>
+
+                    <form method="POST" action="{{ route('admin.order.cancel', ['id' => $order->id]) }}" class="d-inline">
+                        @csrf
+                        <button type="submit" class="btn btn-danger ml-2">Cancel Order</button>
+                    </form>
                 </div>
-                <button type="submit" class="btn btn-primary">Update Status</button>
-            </form>
+            @empty
+                <div class="list-group-item">
+                    <p>No orders found.</p>
+                </div>
+            @endforelse
         </div>
-        <hr>
-    @endforeach
+    </div>
 @endsection
