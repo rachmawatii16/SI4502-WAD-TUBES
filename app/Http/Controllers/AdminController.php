5862c60d3ada19795bfb5660b600c7d25e57509b
ZhafranVI <116346065+ZhafranVI@users.noreply.github.com>
2024-01-01 01:00:57 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -38,5 +38,22 @@ class AdminController extends Controller
             return view('admin.edit', compact('user'));
         }
         return redirect()->back()->with('error', 'Gagal');
+     function update(Request $request, $id){
+        $user = User::find($id);
+        if ($user){
+            $user->name = $request->name;
+            $user->role = $request->role;
+            $user->save();
+            return redirect()->route('admin.home')->with('success', 'User telah berhasil diupdate');
+        }
+        return redirect()->route('admin.home')->with('error', 'Gagal');
+    }
+
+    function edit($id){
+        $user = User::find($id);
+        if ($user) {
+            return view('admin.edit', compact('user'));
+        }
+        return redirect()->back()->with('error', 'Gagal');
     }
 }
