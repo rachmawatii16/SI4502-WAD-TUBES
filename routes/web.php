7b54b347eb5728e428bc25b3be79a6111e613696
ditamlptr <ditaamlptr@gmail.com>
2023-12-31 23:24:30 +0700

    progress lanjutan lagi

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
-    return view('pemesananmakanan');
+    return view('welcome');
+})->name('home');
+
+Route::get('/login', [App\Http\Controllers\AuthManager::class, 'login'])->name('login');
+Route::post('/login', [App\Http\Controllers\AuthManager::class, 'loginPost'])->name('login.post');
+
+Route::get('/registration', [App\Http\Controllers\AuthManager::class, 'registration'])->name('registration');
+Route::post('/registration', [App\Http\Controllers\AuthManager::class, 'registrationPost'])->name('registration.post');
+
+Route::get('/logout', [App\Http\Controllers\AuthManager::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/user/order', [UserController::class, 'showOrderForm'])->name('user.order.form');
+    Route::post('/user/order', [UserController::class, 'createOrder'])->name('user.order');
+    Route::get('/admin/orders', [AdminController::class, 'viewOrders'])->name('admin.orders');
+    Route::put('/admin/order/update/{id}', [AdminController::class, 'updateOrderStatus'])->name('admin.order.update');
+    Route::get('/admin/orders/edit/{id}', [AdminController::class, 'editOrder'])->name('admin.orders.edit');
+    Route::put('/admin/orders/update/{id}', [AdminController::class, 'updateOrder'])->name('admin.orders.update');
 });
